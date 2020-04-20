130ad28b49b45a389c781436e4e80eeb9a0c4dea
bb35319036 <bb35319036@ms.nagasaki-u.ac.jp>
2020-04-20 22:47:46 +0900

    4/20 1回目

--- k01/k01.c
+++ k01/k01.c
@@ -3,33 +3,43 @@
 #include <string.h>
 #include <math.h>
 
-extern double ave_online(double val,double ave)
-extern double var_online()
+extern double ave_online(double ave,double val ,int i)
+{return ((i-1)*ave)/i + val/i;}//平均を求める式を入力
 
+
+extern double var_online(double val, double ave, double square_ave ,int i)
+{
+    return(((i-1)*square_ave/i)+pow(val,2)/i)-pow((((i-1)*ave/i)+(val/i)),2);}//分散を求める式を入力
 int main(void)
 {
-    double val;
+    double ave,val,var,square_ave;
     char fname[FILENAME_MAX];
     char buf[256];
     FILE* fp;
+    double average,Fx;
+    int i,gen;
+    i=0;
+
 
     printf("input the filename of sample:");
     fgets(fname,sizeof(fname),stdin);
     fname[strlen(fname)-1] = '\0';
     printf("the filename of sample: %s\n",fname);
 
-    fp = fopen(fname,"r");
+    fp = fopen(fname,"r");//fileを開いてる　ここでは読み取り専用
     if(fp==NULL){
         fputs("File open error\n",stderr);
         exit(EXIT_FAILURE);
     }
+    fgets(buf,sizeof(buf),fp);
+    while(fgets(buf,sizeof(buf),fp) != NULL)
+    {
+        sscanf(buf,"%d, %lf",&gen, &val);
+        i++;//i=0と宣言しているのでi++を前に置く
+        var=var_online(val,ave,square_ave,i);//自作した分散を求める関数を繰り返すようにここに置く
+        ave=ave_online(ave,val,i);
 
-    while(fgets(buf,sizeof(buf),fp) != NULL){
-        sscanf(buf,"%lf",&val);
-
-
-    
-
+        square_ave=ave_online(square_ave,pow(val,2),i);//（平均,値、n)　分散の式で利用する2乗の平均を求めたいので平均を求める関数をここでも使用する
 
 
     }
@@ -39,9 +49,12 @@ int main(void)
         exit(EXIT_FAILURE);
     }
 
+    Fx=i*var/(i-1);
 
+    printf("ave=%lf\n",ave);
+    printf("var=%lf\n",var);
+
+    printf("unvar=%lf\n",Fx);
     return 0;
-
-
+    
 }
-
