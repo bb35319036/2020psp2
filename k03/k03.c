7da4238a7a0a0f1b547be76db5218b2ac41af758
半田丈都 <>
2020-07-01 13:25:13 +0900

    1

--- k03/k03.c
+++ k03/k03.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define _USE_MATH_DEFINES
 #include <math.h>
 
 extern double r_unif(void);
-extern double r_norm(void);
+extern double r_stdnorm(void);
 
 int main(void)
 {
-    double val;
+    
+    double val, mu,S,S2,Tall;
     char fname[FILENAME_MAX];
     char buf[256];
     FILE* fp;
@@ -27,30 +29,40 @@ int main(void)
     while(fgets(buf,sizeof(buf),fp) != NULL){
         sscanf(buf,"%lf",&val);
 
+    mu=(mu+val)/8;
 
-    
-
-
-
+     }
+     S=0;
+     S2=0;
+     while(fgets(buf,sizeof(buf),fp) != NULL){
+        sscanf(buf,"%lf",&val);
+    S=pow((val-mu),2);
+    S2=(S2+S)/8;
     }
+
 
     if(fclose(fp) == EOF){
         fputs("file close error\n",stderr);
         exit(EXIT_FAILURE);
     }
 
-
+printf("%f\n",S2);
+printf("%f",mu);
+Tall=r_stdnorm*
+ 
+ 
     return 0;
 
 
 }
+
 
 double r_unif(void)
 {
     return (double)(rand()+1)/(RAND_MAX+2);
 }
   
-double r_norm(void)
+double r_stdnorm(void)
 {  
     return sqrt( -2.0*log(r_unif()) ) * sin( 2.0*M_PI*r_unif() );
 } 
